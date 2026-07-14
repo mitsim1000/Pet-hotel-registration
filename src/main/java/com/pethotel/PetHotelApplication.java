8df7784edecfdfd6b395a5d4e2f3944626174f25
mitsim1000 <126378945+mitsim1000@users.noreply.github.com>
2026-07-14 00:56:36 -0600

    Initial project

---
+++ src/main/java/com/pethotel/PetHotelApplication.java
@@ -0,0 +1,17 @@
+package com.pethotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entry point for the pet hotel application
+ *
+ */
+@SpringBootApplication
+public class PetHotelApplication {
+	
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(PetHotelApplication.class, args);
+    }
+}
